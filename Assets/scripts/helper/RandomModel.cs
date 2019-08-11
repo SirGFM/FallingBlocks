839bc479218bd4f246c0a753fbd1103ad7baafe8
SirGFM <gfmandaji@hotmail.com>
2019-08-11 19:38:42 -0300

    prefabs/blocks/BasicBlock: Randomize the front side of the block

---
+++ Assets/scripts/helper/RandomModel.cs
@@ -0,0 +1,15 @@
+﻿using Model = UnityEngine.MeshFilter;
+
+public class RandomModel : UnityEngine.MonoBehaviour {
+    /** List of possible models */
+    public UnityEngine.Mesh[] Models;
+
+    void Start() {
+        Model curModel = this.gameObject.GetComponentInChildren<Model>();
+
+        Global.setup();
+        int idx = Global.PRNG.fastRange(0, Models.Length - 1);
+
+        curModel.mesh = Models[idx];
+    }
+}
