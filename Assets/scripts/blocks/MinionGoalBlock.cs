9b63bbafa18604bb5a8e8aa2307cb4d653399b8d
SirGFM <gfmandaji@hotmail.com>
2019-09-23 15:06:04 -0300

    prefabs/blocks/MinionGoalBlock: Add model and animation for the minion's goal

---
+++ Assets/scripts/blocks/MinionGoalBlock.cs
@@ -0,0 +1,27 @@
+﻿public class MinionGoalBlock : UnityEngine.MonoBehaviour {
+    /* Default size of the light */
+    private const float defaultRadius = 1.5f;
+    /* The halo object */
+    private UnityEngine.Light halo;
+    /* Modifies the radius in 1 unit */
+    private float radius;
+
+    void Start() {
+        this.halo = this.gameObject.GetComponentInChildren<UnityEngine.Light>();
+        this.radius = 0.0f;
+    }
+
+    // Update is called once per frame
+    void Update() {
+        float y;
+
+        this.radius += UnityEngine.Time.deltaTime;
+        if (this.radius > 1.0f)
+            this.radius -= 1.0f;
+
+        /* Use a parabola to modify the halo's radius */
+        y = this.radius;
+        y = 4 * y * (1.0f - y);
+        this.halo.range = y + MinionGoalBlock.defaultRadius;
+    }
+}
