443357d88dc6f2366f0d8b13aafb4ec17e6a3071
SirGFM <gfmandaji@hotmail.com>
2019-11-15 07:31:31 -0300

    scripts/base/GetType: Add behaviour that reports the object's type

---
+++ scripts-v2/base/GetType.cs
@@ -0,0 +1,21 @@
+using EvSys = UnityEngine.EventSystems;
+
+public interface RemoteGetType : EvSys.IEventSystemHandler {
+    void Get(out GetType.Type type);
+}
+
+public class GetType : UnityEngine.MonoBehaviour, RemoteGetType {
+    public enum Type {
+        Error = 0,
+        None,
+        Minion,
+        Player,
+        Goal,
+    }
+
+    public Type type = Type.None;
+
+    public void Get(out Type outType) {
+        outType = this.type;
+    }
+}
