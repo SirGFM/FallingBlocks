1d5c13167607b8ec132066809501597add6131b7
SirGFM <gfmandaji@hotmail.com>
2019-11-17 16:23:04 -0300

    scripts/level/InitialPlayerPosition: Spawn camera and player, if on editor

--- Assets/scripts/level/InitialPlayerPosition.cs
+++ Assets/scripts/level/InitialPlayerPosition.cs
@@ -1,7 +1,14 @@
 using GO = UnityEngine.GameObject;
+using Obj = UnityEngine.Object;
+using Quat = UnityEngine.Quaternion;
 using Vec3 = UnityEngine.Vector3;
 
 public class InitialPlayerPosition : BaseRemoteAction {
+    /** DEBUG ONLY:The player prefab */
+    public GO dbgPlayer;
+    /** DEBUG ONLY:The main camera */
+    public GO dbgCamera;
+
     protected int checkPointIdx;
 
     void Start() {
@@ -20,8 +27,20 @@ public class InitialPlayerPosition : BaseRemoteAction {
         yield return null;
 
         p = this.transform.position;
+
+        if (UnityEngine.Application.isEditor &&
+                UnityEngine.Camera.main == null) {
+            /* XXX: If testing a particular scene, it won't have neither the
+             * camera nor the player (as those are maintained by the loader
+             * scene)... So spawn them here. */
+            Obj.Instantiate(this.dbgCamera, new Vec3(), Quat.identity);
+            Obj.Instantiate(this.dbgPlayer, p, Quat.identity);
+        }
+
         this.rootEvent<LoaderEvents>(
                 (x,y) => x.SetCheckpointPosition(this.checkPointIdx, p) );
+
+        this.destroy();
     }
 
     private System.Collections.IEnumerator goDestroy() {
