dc7f9e1c499f086190765289269d795c45ef251a
SirGFM <gfmandaji@hotmail.com>
2020-03-21 11:13:45 -0300

    scenes/000-game-controller/Options: Correctly initialize camera checkboxes

--- Assets/scripts/ui/SetCamera.cs
+++ Assets/scripts/ui/SetCamera.cs
@@ -1,14 +1,31 @@
 using UiToggle = UnityEngine.UI.Toggle;
 
 public class SetCamera : UnityEngine.MonoBehaviour {
-    public void SetHorizontal(UiToggle toggle) {
+    public bool isHorizontal = true;
+
+    void Start() {
+        UiToggle toggle = this.GetComponent<UiToggle>();
+        if (isHorizontal)
+            toggle.isOn = (Global.camX == -1.0f);
+        else
+            toggle.isOn = (Global.camY == -1.0f);
+    }
+
+    public void SetCameraAxis(UiToggle toggle) {
+        if (this.isHorizontal)
+            this.SetHorizontal(toggle);
+        else
+            this.SetVertical(toggle);
+    }
+
+    private void SetHorizontal(UiToggle toggle) {
         if (toggle.isOn)
             Global.camX = -1.0f;
         else
             Global.camX = 1.0f;
     }
 
-    public void SetVertical(UiToggle toggle) {
+    private void SetVertical(UiToggle toggle) {
         if (toggle.isOn)
             Global.camY = -1.0f;
         else
