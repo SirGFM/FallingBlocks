10ef407fe831bec8a3af5027bad3c51a5bfe9527
SirGFM <gfmandaji@hotmail.com>
2020-03-24 07:33:33 -0300

    scripts/ui/ConfigureInput: Set timeout to stop waiting for inputs

--- Assets/scripts/ui/ConfigureInput.cs
+++ Assets/scripts/ui/ConfigureInput.cs
@@ -1,12 +1,35 @@
+using CoroutineRet = System.Collections.IEnumerator;
 using UiButton = UnityEngine.UI.Button;
 using UiText = UnityEngine.UI.Text;
 
 public class ConfigureInput : UnityEngine.MonoBehaviour {
     private UiText label;
-    private bool waiting;
+    private UnityEngine.Coroutine bgFunc;
 
     public Input.Actions action;
     public int column;
+
+    private CoroutineRet WaitTimeout() {
+        for (int time = 5; time >= 0; time--) {
+            this.label.text = $"Waiting input... {time}...";
+
+            for (int i = 0; i < 100; i++) {
+                yield return new UnityEngine.WaitForSeconds(0.01f);
+
+                if (!Input.IsWaitingInput()) {
+                    /* Done waiting! */
+                    string axisName;
+                    axisName = Input.AxisName(this.action, this.column);
+                    this.label.text = axisName;
+                    this.StopCoroutine(this.bgFunc);
+                }
+            }
+        }
+
+        this.label.text = "";
+        Input.CancelWaitInput();
+        Input.ClearAxis(this.action, this.column);
+    }
 
     private void OnClick() {
         if (Input.IsWaitingInput())
@@ -14,7 +37,7 @@ public class ConfigureInput : UnityEngine.MonoBehaviour {
 
         this.label.text = "Waiting input...";
 
-        this.waiting = true;
+        this.bgFunc = this.StartCoroutine(this.WaitTimeout());
         Input.WaitInput(this.gameObject, this.column, this.action);
     }
 
@@ -24,16 +47,5 @@ public class ConfigureInput : UnityEngine.MonoBehaviour {
 
         this.label = this.GetComponentInChildren<UiText>();
         this.label.text = Input.AxisName(this.action, this.column);
-        this.waiting = false;
-    }
-
-    void Update() {
-        if (!this.waiting)
-            return;
-
-        if (!Input.IsWaitingInput()) {
-            /* Done waiting! */
-            this.label.text = Input.AxisName(this.action, this.column);
-        }
     }
 }
